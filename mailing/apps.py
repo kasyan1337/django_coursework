2e9fff5fa7c4b24447cf744e2438f4c2a9617f29
kasyan1337 <tristokorun@gmail.com>
2024-06-20 02:24:01 +0800

    tasks.py - background schedulder named start and send_mailing logic
    apps.py - start the scheduler when the app is ready

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,6 +1,14 @@
+from time import sleep
+
 from django.apps import AppConfig
+
+from mailing.tasks import start
 
 
 class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
+
+    def ready(self):  # added manually
+        sleep(2)
+        start()  # start the scheduler when the app is ready, custom function from tasks module
